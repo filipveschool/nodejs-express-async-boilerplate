d9d89dce45e3858bab8d0c6d6e99893ca5c31df4
Filip Vanden Eynde <filipve@outlook.com>
2018-10-31 10:38:53 +0100

    update

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  extends: ['airbnb-base', 'prettier',],
+  plugins: ['prettier'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'arrow-body-style': [2, 'as-needed'],
+    'class-methods-use-this': 0,
+    'comma-dangle': [2, 'always-multiline'],
+    'import/imports-first': 0,
+    'import/newline-after-import': 0,
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/no-unresolved': 2,
+    'import/no-webpack-loader-syntax': 0,
+    'import/prefer-default-export': 0,
+
+  },
+};
--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/* eslint-disable import/order */
+const debug = require('debug')('nodejs-express-async-boilerplate:server');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const compression = require('compression');
+const consolelogger = require('./server/logger');
+const argv = require('./server/argv');
+const port = require('./server/port');
+const isDev = process.env.NODE_ENV !== 'production';
+const ngrok =
+  (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel
+    ? require('ngrok')
+    : false;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+// #####################################################################################################################
+// #####################################################################################################################
+// COMPRESSION
+// #####################################################################################################################
+// #####################################################################################################################
+app.use(compression())
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +32,55 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// #####################################################################################################################
+// #####################################################################################################################
+// ROUTES
+// #####################################################################################################################
+// #####################################################################################################################
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+// #####################################################################################################################
+// #####################################################################################################################
+// get the intended host and port number, use localhost and port 3000 if not provided
+// #####################################################################################################################
+// #####################################################################################################################
+const customHost = argv.host || process.env.HOST;
+const host = customHost || null; // Let http.Server use its default IPv6/4 host
+const prettyHost = customHost || 'localhost';
+
+// #####################################################################################################################
+// #####################################################################################################################
+// use the gzipped bundle
+// #####################################################################################################################
+// #####################################################################################################################
+// app.get('*.js', (req, res, next) => {
+//   req.url = req.url + '.gz'; // eslint-disable-line
+//   res.set('Content-Encoding', 'gzip');
+//   next();
+// });
+
+// #####################################################################################################################
+// #####################################################################################################################
+// START YOUR APP
+// #####################################################################################################################
+// #####################################################################################################################
+
+app.listen(port, host, async err => {
+  if (err) {
+    return consolelogger.error(err.message);
+  }
+
+  // Connect to ngrok in dev mode
+  if (ngrok) {
+    let url;
+    try {
+      url = await ngrok.connect(port);
+    } catch (e) {
+      return consolelogger.error(e);
+    }
+    consolelogger.appStarted(port, prettyHost, url);
+  } else {
+    consolelogger.appStarted(port, prettyHost);
+  }
+});
